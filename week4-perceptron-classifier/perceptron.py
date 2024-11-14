5e2d9e37a285d3494d3302645bfe34463e224212
aysukoksal <aysu.koksal7@gmail.com>
2024-11-14 16:47:15 +0100

    completed the coding part for 4

--- week4-perceptron-classifier/perceptron.py
+++ week4-perceptron-classifier/perceptron.py
@@ -13,16 +13,18 @@ class Percep:
         self.activation_func= unit_step_func
         self.weights= None
         self.bias= None
+        self.ber_score=None
+        self.accuracy_score=None
     
-    def fit(self, X,y):
-        n_samples, n_features= X.shape
+    def fit(self, X_train,y_train):
+        n_samples, n_features= X_train.shape
         self.weights= np.random.rand(n_features)
         self.bias=0
-        y_ = np.where(y>0, 1, 0)
+        y_ = np.where(y_train>0, 1, 0)
         #learn weights
 
         for _ in range(self.n_iters):
-            for idx, x_i in enumerate(X):
+            for idx, x_i in enumerate(X_train):
                 linear_output=np.dot(x_i, self.weights) + self.bias
                
                 y_predicted= self.activation_func(linear_output)
@@ -31,7 +33,33 @@ class Percep:
                 update= self.lr * (y_[idx]-y_predicted)
                 self.weights+= update*x_i
                 self.bias+=update
+        
+    def BER(self, y_test, y_pred):
+
+        is_negative_one= y_test==-1 #this creates a bool array where true when y_true is -1
+        is_positive_one = y_test==1 #this creates a bool array where true when y_true is 1
+
+
+        #y_true==y_pred returns a list of boolean values where true indicates elements are equal and false elements are not equal
+        t_minus= np.sum((y_test==y_pred)& is_negative_one)  #when we sum an array true values are 1 and false 0. so this returns number of times where y_true==y_pred
+        f_minus =np.sum((y_test!=y_pred)& is_negative_one) #~ is bitwise NOT and flip all the boolean values
+        t_pos= np.sum((y_test==y_pred)& is_positive_one)
+        f_pos= np.sum((y_test!=y_pred)& is_positive_one)
+        ber_score= 0.5*(f_minus/(f_minus+t_minus) + f_pos/(f_pos+t_pos))
+        self.ber_score=ber_score
+
+    def accuracy(self, y_test, y_pred):
+        accuracy_score = np.sum(y_test == y_pred) / len(y_test)
+        self.accuracy_score=accuracy_score
+    
     def predict(self, X):
         linear_output=np.dot(X, self.weights) + self.bias
         y_predicted = self.activation_func(linear_output)
         return y_predicted
+
+    def create_percep(self, X_train, y_train, X_test,y_test):
+        self.fit(X_train, y_train)
+        predictions = self.predict(X_test)
+        self.accuracy(y_test, predictions)
+        self.BER(y_test, predictions)
+        return self
--- week4-perceptron-classifier/testing.py
+++ week4-perceptron-classifier/testing.py
@@ -22,34 +22,74 @@ if __name__ == "__main__":
     X_test = X_test.to_numpy(dtype=float)
     y_test = y_test.to_numpy(dtype=float)
 
-    def accuracy(y_true, y_pred):
-        accuracy = np.sum(y_true == y_pred) / len(y_true)
-        return accuracy
+   
+    
+    def plot_training(p, X_train):
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        plt.scatter(X_train[:, 0], X_train[:, 1], marker="o", c=y_train)
+
+        x0_1 = np.amin(X_train[:, 0])
+        x0_2 = np.amax(X_train[:, 0])
+
+        x1_1 = (-p.weights[0] * x0_1 - p.bias) / p.weights[1]
+        x1_2 = (-p.weights[0] * x0_2 - p.bias) / p.weights[1]
+
+        ax.plot([x0_1, x0_2], [x1_1, x1_2], "k")
+
+        ymin = np.amin(X_train[:, 1])
+        ymax = np.amax(X_train[:, 1])
+        ax.set_ylim([ymin - 3, ymax + 3])
+
+        plt.show()
+    
+    def plot_test(p, X_test):
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        plt.scatter(X_test[:, 0], X_test[:, 1], marker="o", c=y_test)
+
+        x0_1 = np.amin(X_test[:, 0])
+        x0_2 = np.amax(X_test[:, 0])
+
+        x1_1 = (-p.weights[0] * x0_1 - p.bias) / p.weights[1]
+        x1_2 = (-p.weights[0] * x0_2 - p.bias) / p.weights[1]
+
+        ax.plot([x0_1, x0_2], [x1_1, x1_2], "k")
+
+        ymin = np.amin(X_test[:, 1])
+        ymax = np.amax(X_test[:, 1])
+        ax.set_ylim([ymin - 3, ymax + 3])
+
+        plt.show()
+    p1= Percep().create_percep(X_train, y_train, X_test,y_test)
+    p2= Percep().create_percep(X_train, y_train, X_test,y_test)
+    p3= Percep().create_percep(X_train, y_train, X_test,y_test)
+    p4= Percep().create_percep(X_train, y_train, X_test,y_test)
+    p5= Percep().create_percep(X_train, y_train, X_test,y_test)
+    p6= Percep().create_percep(X_train, y_train, X_test,y_test)
+    p7= Percep().create_percep(X_train, y_train, X_test,y_test)
+    p8= Percep().create_percep(X_train, y_train, X_test,y_test)
+    p9= Percep().create_percep(X_train, y_train, X_test,y_test)
+    p10= Percep().create_percep(X_train, y_train, X_test,y_test)
+    
+    perceps=[p1,p2,p3,p4,p5,
+                p6,p7,p8,p9,p10]
+    sorted_objects = sorted(perceps, key=lambda obj: obj.ber_score)
+    lowest_ber= sorted_objects[0]
+    
+    print(f"Lowest ber perceptron weights: {lowest_ber.weights}")
+
+    sorted_objects = sorted(perceps, key=lambda obj: obj.ber_score, reverse=True)
+    best_ber= sorted_objects[0]
+    plot_test(best_ber, X_test)
+    plot_training(best_ber, X_train)
+    
+    
+    
 
 
-    p = Percep(learning_rate=0.01, n_iters=1000)
-    p.fit(X_train, y_train)
-    predictions = p.predict(X_test)
 
-    print("Perceptron classification accuracy", accuracy(y_test, predictions))
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    plt.scatter(X_train[:, 0], X_train[:, 1], marker="o", c=y_train)
-
-    x0_1 = np.amin(X_train[:, 0])
-    x0_2 = np.amax(X_train[:, 0])
-
-    x1_1 = (-p.weights[0] * x0_1 - p.bias) / p.weights[1]
-    x1_2 = (-p.weights[0] * x0_2 - p.bias) / p.weights[1]
-
-    ax.plot([x0_1, x0_2], [x1_1, x1_2], "k")
-
-    ymin = np.amin(X_train[:, 1])
-    ymax = np.amax(X_train[:, 1])
-    ax.set_ylim([ymin - 3, ymax + 3])
-
-    plt.show()
+ 
 
 
 
